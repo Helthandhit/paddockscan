ed50a5b3d8d4caacd5eacc586818d39ddd9c9777
Helthandhit <Healthandhit@gmail.com>
2026-07-15 14:29:20 +0100

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD9Dbg3jSaoXd8wWObgsVKv_2g2L-tpbZ8",
-  authDomain: "paddockscan.firebaseapp.com",
-  projectId: "paddockscan",
-  storageBucket: "paddockscan.firebasestorage.app",
-  messagingSenderId: "467483184710",
-  appId: "1:467483184710:web:6936967ac1ca259c6c4447",
-  measurementId: "G-JKX1XVX9L1"
+window.PADDOCKSCAN_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyD9Dbg3jSaoXd8wWObgsVKv_2g2L-tpbZ8",
+    authDomain: "paddockscan.firebaseapp.com",
+    projectId: "paddockscan",
+    storageBucket: "paddockscan.firebasestorage.app",
+    messagingSenderId: "467483184710",
+    appId: "1:467483184710:web:6936967ac1ca259c6c4447",
+    measurementId: "G-JKX1XVX9L1"
+  },
+  appCheckSiteKey: "",
+  domain: "paddockscan.com",
+  contactEmail: "paddockscan@gmail.com",
+  playStoreUrl: "https://play.google.com/apps/testing/com.healthandhit.paddockscan"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
